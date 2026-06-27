abe6b8222019d0fe5e06f23bb5344fd7702ff1fd
Fernando E. <fernando.espinosa@payclip.com>
2026-06-26 18:40:57 -0600

    feat: inicializar proyecto Next.js 15 con Tailwind v4

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+// next.config.ts
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig

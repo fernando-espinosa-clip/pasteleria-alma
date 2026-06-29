be3a362496dcccd7a0edaf8d0ab25b0367fb9560
Fernando E. <fernando.espinosa@payclip.com>
2026-06-29 12:47:32 -0600

    style: mover PromoSection arriba de FeaturedCakes en home

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <>
       <Hero />
       <TrustBar />
-      <FeaturedCakes />
       <PromoSection />
+      <FeaturedCakes />
       <HomeCTA />
     </>
   )
